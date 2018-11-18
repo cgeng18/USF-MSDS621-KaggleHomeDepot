e046fcf83eef9a088abe36bda809465e11a7fe63
robertisandor <robert.i.sandor@gmail.com>
2018-11-17 18:26:35 -0800

    Integrated Chong's additions from code_V2 with some edits

--- search_terms_description.py
+++
@@ -1,8 +0,0 @@
-from search_parser import *
-
-search_string = 'this is test input'
-dictionary = make_dictionary(glove_file)
-results = find_ten(search_string.split(),dictionary)
-
-for word in search_string.split():
-    print(word+':'+str(results[word]))
